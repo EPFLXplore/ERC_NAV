7d4a5e318e03ef4f8f479886dc5dec970529a322
hmiranda-queiros <hugomirandaqueiros@gmail.com>
2024-02-26 15:49:46 +0100

    feat: add gazebo simulation and inertial test

--- rover_description/launch/display.launch.py
+++ rover_description/launch/display.launch.py
@@ -5,18 +5,25 @@ import os
 
 
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package="path_planning").find(
-        "path_planning"
-    )
+    pkg_share = launch_ros.substitutions.FindPackageShare(
+        package="rover_description"
+    ).find("rover_description")
     default_model_path = os.path.join(pkg_share, "src/description/rover.urdf")
     default_rviz_config_path = os.path.join(pkg_share, "rviz/urdf_config.rviz")
+
+    spawn_entity = launch_ros.actions.Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-entity", "rover", "-topic", "robot_description"],
+        output="screen",
+    )
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         parameters=[{"robot_description": Command(["xacro ", default_model_path])}],
     )
-    
+
     joint_state_publisher_node = launch_ros.actions.Node(
         package="joint_state_publisher",
         executable="joint_state_publisher",
@@ -33,5 +40,21 @@ def generate_launch_description():
     )
 
     return launch.LaunchDescription(
-        [rviz_node, robot_state_publisher_node, joint_state_publisher_node]
+        [
+            launch.actions.ExecuteProcess(
+                cmd=[
+                    "gazebo",
+                    "--verbose",
+                    "-s",
+                    "libgazebo_ros_init.so",
+                    "-s",
+                    "libgazebo_ros_factory.so",
+                ],
+                output="screen",
+            ),
+            spawn_entity,
+            rviz_node,
+            robot_state_publisher_node,
+            joint_state_publisher_node,
+        ]
     )
