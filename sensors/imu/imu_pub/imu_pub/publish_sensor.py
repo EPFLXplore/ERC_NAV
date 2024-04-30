360d3178e7cb9e1673dcad82a2225f7800cdfd16
rimelq <rim.elqabli@epfl.ch>
2024-04-30 23:31:31 +0200

    Fix publisher, update setup file

--- sensors/imu/imu_pub/imu_pub/publish_sensor.py
+++ sensors/imu/imu_pub/imu_pub/publish_sensor.py
@@ -5,68 +5,100 @@ import serial
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Quaternion, Vector3
 from std_msgs.msg import Header
-ser = Serial.serial('dev/ttyACM0', 115200)
+ser = serial.Serial('/dev/ttyACM0', 115200)
+
 
 class MinimalPublisher(Node):
 
+    pitch = float(0.0)
+    yaw = float(0.0)
+    roll = float(0.0)
+
+    acc_x = float(0.0)
+    acc_y = float(0.0)
+    acc_z = float(0.0)
+
+    mag_x = float(0.0)
+    mag_y = float(0.0)
+    mag_z = float(0.0)
+
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(Imu, 'topic', 10)
+        self.publisher_ = self.create_publisher(Imu, 'Imu', 10)
         timer_period = 0.005  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
         
-        msg_raw = ser.read().split(' ')
+        msg_raw = ser.readline().split(b' ')
         imu_msg = Imu()
-        header  = Header()
+        header  = Header(stamp = self.get_clock().now().to_msg(), frame_id = "av_imu")
         orientation_quat = Quaternion()
-        orientation_cov = [-1, 0, 0,
-                            0, 0 ,0,
-                            0, 0 ,0]
-        angular_cov = [0, 0, 0,
-                       0, 0 ,0,
-                       0, 0 ,0]
-        linear_cov = [0, 0, 0,
-                      0, 0, 0, 
-                      0, 0 ,0]
 
-        # Supposed data "[verif_token], ax, ay, az, gx, ..., mx,..."
-        if(msg_raw[0].lower() == 'imu'):
-            acc_x = float(msg_raw[1])
-            acc_y = float(msg_raw[2])
-            acc_z = float(msg_raw[3])
-            pitch = float(msg_raw[4])
-            yaw = float(msg_raw[5])
-            roll = float(msg_raw[6])
-            mag_x = float(msg_raw[7])
-            mag_y = float(msg_raw[8])
-            mag_z = float(msg_raw[9]) 
+        if(msg_raw[0].lower() == b'imu'):
+            self.acc_x = float(msg_raw[1])
+            self.acc_y = float(msg_raw[2])
+            self.acc_z = float(msg_raw[3])
+            self.pitch = float(msg_raw[4])
+            self.yaw = float(msg_raw[5])
+            self.roll = float(msg_raw[6])
+            self.mag_x = float(msg_raw[7])
+            self.mag_y = float(msg_raw[8])
+            self.mag_z = float(msg_raw[9]) 
 
-        angular_acc = Vector3(pitch, yaw, roll)
-        linear_acc = Vector3(acc_x, acc_y, acc_z)
+        pitch = self.pitch
+        yaw = self.yaw
+        roll = self.roll
+
+        acc_x = self.acc_x
+        acc_y = self.acc_y
+        acc_z = self.acc_z
+
+        mag_x = self.mag_x    
+        mag_y = self.mag_y    
+        mag_z = self.mag_z    
 
         qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
         qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
         qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
         qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
  
- 
+        orientation_quat.x = qx
+        orientation_quat.y = qy
+        orientation_quat.z = qz
+        orientation_quat.w = qw
+
+        # orientation_cov = [-1, 0, 0,
+        #                     0, 0 ,0,
+        #                     0, 0 ,0]
+        # angular_cov = [0, 0, 0,
+        #                0, 0 ,0,
+        #                0, 0 ,0]
+        # linear_cov = [0, 0, 0,
+        #               0, 0, 0, 
+        #               0, 0 ,0]
+
+        # Supposed data "[verif_token], ax, ay, az, gx, ..., mx,..."
+
+        angular_acc = Vector3(x=pitch, y=yaw, z=roll)
+        linear_acc = Vector3(x=acc_x, y=acc_y, z=acc_z)
+
+
         imu_msg.header = header
 
         imu_msg.orientation = orientation_quat
-        imu_msg.orientation_covariance = orientation_cov
-        
+        #imu[-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]_msg.orientation_covariance = [-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        imu_msg.orientation_covariance = np.zeros(9, dtype=np.float64)
+
         imu_msg.angular_velocity = angular_acc
-        imu_msg.angular_velocity_covariance = angular_cov
+        imu_msg.angular_velocity_covariance = np.zeros(9, dtype=np.float64)
         
         imu_msg.linear_acceleration = linear_acc
-        imu_msg.linear_acceleration_covariance = linear_cov
+        imu_msg.linear_acceleration_covariance = np.zeros(9, dtype=np.float64)
         
 
         self.publisher_.publish(imu_msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
 
 
--- sensors/imu/imu_pub/setup.py
+++ sensors/imu/imu_pub/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'imu_pub'
@@ -10,16 +12,26 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='rimelq',
     maintainer_email='rim.elqabli@epfl.ch',
+    # keywords=['foo', 'bar'],
+    # classifiers=[
+    #     'Intended Audience :: Developers',
+    #     'License :: TODO',
+    #     'Programming Language :: Python',
+    #     'Topic :: Software Development',
+    # ],
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'talker = imu_pub.publish_sensor:main',
         ],
     },
 )
