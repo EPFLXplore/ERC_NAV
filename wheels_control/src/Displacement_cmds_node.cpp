b6808807bda8e25fba7b956e3010fc2eab4f89de
ahmeddnadim <ahmed.nadim@epfl.ch>
2024-05-14 16:32:11 +0200

    bugs

--- wheels_control/src/Displacement_cmds_node.cpp
+++ wheels_control/src/Displacement_cmds_node.cpp
@@ -158,7 +158,8 @@ class DisplacementCmds : public rclcpp::Node
         else if (msg->data == "lateral")  
         {
           //kinematic_state = LATERAL_KINEMATIC;
-        else if ( msg- >data = "crab" )
+        }
+        else if ( msg->data = "crab" )
         {
           kinematic_state = CRAB_KINEMATIC;
         }
