fcbb3861396db807a1688e79be72339f45ee9dc3
ahmeddnadim <ahmed.nadim@epfl.ch>
2024-05-16 16:28:54 +0200

    bugs

--- wheels_control/src/kinematic_models/stby_kinematic_model.cpp
+++ wheels_control/src/kinematic_models/stby_kinematic_model.cpp
@@ -37,7 +37,13 @@ motors_obj RoverStbyKinematicModel::run(motors_obj motors_position, _Float64 v_x
     rotation(alpha);
     if(check_steering_position_for_rotation(current_motors_position)){
         translation(speed);
-    }
+    }else{
+
+        current_motors_cmds.drive[FRONT_LEFT] = 0;
+        current_motors_cmds.drive[FRONT_RIGHT] = 0;
+        current_motors_cmds.drive[BACK_RIGHT] = 0;
+        current_motors_cmds.drive[BACK_LEFT] = 0;
+     }
     
     current_motors_cmds.info = "translation only";
     return current_motors_cmds;
@@ -50,19 +56,12 @@ motors_obj RoverStbyKinematicModel::run(motors_obj motors_position, _Float64 v_x
 
 void RoverStbyKinematicModel::translation(_Float64 norm_speed) 
 {
-    if(current_motors_cmds.steer[FRONT_LEFT] == 90 && current_motors_cmds.steer[FRONT_RIGHT] == 90 && current_motors_cmds.steer[BACK_LEFT] == 90 && current_motors_cmds.steer[BACK_RIGHT] == 90 ){
+    
     /* Commands motors for just a basic translation */
     current_motors_cmds.drive[FRONT_LEFT] = norm_speed;
     current_motors_cmds.drive[FRONT_RIGHT] = norm_speed;
     current_motors_cmds.drive[BACK_RIGHT] = norm_speed;
     current_motors_cmds.drive[BACK_LEFT] = norm_speed;
-    }else{
-        current_motors_cmds.drive[FRONT_LEFT] = 0;
-        current_motors_cmds.drive[FRONT_RIGHT] = 0;
-        current_motors_cmds.drive[BACK_RIGHT] = 0;
-        current_motors_cmds.drive[BACK_LEFT] = 0;
-    }
-
 }
 
 
@@ -91,7 +90,7 @@ bool RoverStbyKinematicModel::check_steering_position_for_rotation(motors_obj cu
 
     for (auto motor = 0; motor < NB_WHEELS; motor++) 
     {
-        if (std::abs(std::abs(current_motors_position.steer[motor]) - 90) < 2)  // 50 cest un petit interval et 90 cest langle quon veut 
+        if (std::abs(std::abs(current_motors_position.steer[motor]) - 90) < 50)  // 50 cest un petit interval et 90 cest langle quon veut 
         {
             wheel_positioned_for_rotation++;
         }
