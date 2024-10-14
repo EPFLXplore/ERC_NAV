33fb987e10a3c8df210b76d430f1196c6503a81b
flxinxout <giovanni.ranieri@epfl.ch>
2024-10-14 20:37:09 +0200

    new on start up

---
+++ start_wheels.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd /home/brokkoly/Desktop/ERC_NAV/docker_humble_jetson
+./start_wheels_docker.sh
---
+++ stop_wheels.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+/usr/bin/docker stop nav_humble_jetson
+# need to test if the motors are going into their state of undonfigured!
