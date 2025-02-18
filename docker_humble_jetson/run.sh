cb664194674bc29e3ef55f37bdb1d6e354f1be4b
epfl-xplore <se.software@epfl-xplore.ch>
2025-02-18 22:31:17 +0100

    cleaned up dockerfile

--- docker_humble_jetson/run.sh
+++ docker_humble_jetson/run.sh
@@ -25,6 +25,7 @@ ls -FAlh $XAUTH
 echo ""
 echo "Running docker..."
 
+
 # Get the current working directory
 current_dir=$(pwd)
 
@@ -33,14 +34,20 @@ parent_dir=$(dirname "$current_dir")
 
 USERNAME=xplore
 
+
 docker run -it \
     --name nav_humble_jetson \
     --rm \
     --privileged \
     --net=host \
+    --runtime=nvidia \
+    --gpus all \
     -e DISPLAY=unix$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
     -e XAUTHORITY=$XAUTH \
+    --env GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11 \
+    --env GAZEBO_MODEL_PATH=/usr/share/gazebo-11/models \
+    --env GAZEBO_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/gazebo-11/plugins \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v $XAUTH:$XAUTH \
     -v /run/user/1000/at-spi:/run/user/1000/at-spi \
@@ -49,4 +56,4 @@ docker run -it \
     -v nav_humble_jetson_home_volume:/home/xplore \
     --add-host=os-122140001125.local:169.254.55.220 \
     ghcr.io/epflxplore/nav:humble-jetson \
-    /bin/bash -c "sudo chown -R $USERNAME:$USERNAME /home/$USERNAME; /bin/bash"
+    /bin/bash -c "sudo chown -R $USERNAME:$USERNAME /home/$USERNAME; export PYTHONPATH=/home/xplore/dev_ws/install/rover_pkg/lib/python3.10/site-packages:/home/xplore/dev_ws/install/custom_msg/local/lib/python3.10/dist-packages:/opt/ros/humble/install/local/lib/python3.10/dist-packages:/opt/ros/humble/install/lib/python3.10/site-packages:/opt/ros/humble/local/lib/python3.10/dist-packages:/opt/ros/humble/lib/python3.10/site-packages; /bin/bash"
