9eb5d32225dfa472029240f75995b3ccc85df36d
Edgard <halledgard@gmail.com>
2025-11-29 15:30:28 +0000

    centre_cube_essai

--- localization/camera/ros2_aruco_with_lidar/ros2_aruco_with_lidar/detect_cube.cpp
+++ localization/camera/ros2_aruco_with_lidar/ros2_aruco_with_lidar/detect_cube.cpp
@@ -11,6 +11,8 @@
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/filter.h>
 #include <vector>
+#include <cmath>
+
 // #include <pcl/filters/project_inliers.h>
 
 
@@ -24,9 +26,9 @@ public:
         output_cloud_topic_ = "/cloud_without_lines";
         distance_threshold_inliers = 0.01; 
         max_iterations_ = 300;
-        t=0.3; //longeur des normes des coté du cube 
+        t=0.25; //longeur des normes des coté du cube 
         min_inliers_ = 30; // at least 20 points to define a line
-        max_lines_ = 5; // at least 2 lines to form a corner
+        max_lines_ = 3; // at least 2 lines to form a corner
         
         
         auto qos = rclcpp::QoS(rclcpp::KeepLast(1)).best_effort();
@@ -39,6 +41,8 @@ public:
         );
         lines_pub_ = create_publisher<sensor_msgs::msg::PointCloud2>(output_cloud_topic_, qos);
         markers_pub_ = create_publisher<visualization_msgs::msg::MarkerArray>("detected_lines_markers", qos);
+        centre_pub_ = create_publisher<visualization_msgs::msg::MarkerArray>("centre_cube_beleck", qos);
+
 
         RCLCPP_INFO(this->get_logger(), "initialized detect_cube_node");
 
@@ -99,7 +103,34 @@ private:
             float dx = coefficients->values[3];
             float dy = coefficients->values[4];
             float dz = coefficients->values[5];
+    
+            float norm_dir = sqrt(dx * dx + dy * dy + dz * dz);
             
+            float ndx = dx / norm_dir;
+            float ndy = dy / norm_dir;
+            float ndz = dz / norm_dir;
+
+            bool is_parallel = false;
+            const float parallel_cos_threshold = 0.99; // 8degre a peut pres
+
+            for (const auto &L : lines) {
+                float norm_existing = sqrt(L.dx * L.dx + L.dy * L.dy + L.dz * L.dz);
+                if (norm_existing == 0.0f) {
+                    continue;
+                }
+                float ex = L.dx / norm_existing;
+                float ey = L.dy / norm_existing;
+                float ez = L.dz / norm_existing;
+
+                float dot = ndx * ex + ndy * ey + ndz * ez;
+                if (fabs(dot) > parallel_cos_threshold) {
+                    is_parallel = true;
+                    RCLCPP_INFO(this->get_logger(), "ligneparalele");
+
+                    break;
+                }
+
+            }
             
 
             // Extraire d'abord tous les inliers de cette ligne
@@ -110,84 +141,85 @@ private:
             pcl::PointCloud<pcl::PointXYZ>::Ptr detected_lines(new pcl::PointCloud<pcl::PointXYZ>());
             extract.filter(*detected_lines);
 
-            
-            *all_inliers += *detected_lines;
+            if (!is_parallel) {
+                *all_inliers += *detected_lines;
 
-            geometry_msgs::msg::Point p_first, p_last;
-            //je prend ma ligne qui commence a xo,y,zo 
-            //a changer parce que ca donne des segment decallé mais sur la bonne direction juste pour check 
-            float norm = sqrt(dx*dx + dy*dy + dz*dz);
-            p_first.x = x0;
-            p_first.y = y0;
-            p_first.z = z0;
-            p_last.x = p_first.x + dx*t/norm;
-            p_last.y= p_first.y + dy*t/norm;
-            p_last.z= p_first.z + dz*t/norm;
-
-
-
-            // pcl::PointCloud<pcl::PointXYZ> projected_inliers;
-            // pcl::ProjectInliers<pcl::PointXYZ> proj;
-            // proj.setModelType(pcl::SACMODEL_LINE);
-            // proj.setInputCloud(detected_lines);
-            // proj.setModelCoefficients(coefficients);
-            // proj.filter(projected_inliers);
-
-            // std::vector<float> ts;
-            // ts.reserve(projected_inliers.size());
-
-            // for (const auto &p : projected_inliers.points) {
-            //     float vx = p.x - x0;
-            //     float vy = p.y - y0;
-            //     float vz = p.z - z0;
-
-            //     float t = vx * dx + vy * dy + vz * dz;
-            //     ts.push_back(t);
-            // }
-            // auto [t_min_it, t_max_it] = std::minmax_element(ts.begin(), ts.end());
-            //     float t_min = *t_min_it;
-            //     float t_max = *t_max_it;
-            
-            //geometry_msgs::msg::Point p_first, p_last,p_milieu;
-            // p_first.x = x0 + t_min * dx/norm;
-            // p_first.y = y0 + t_min * dy/norm;
-            // p_first.z = z0 + t_min * dz/norm;
-
-            // p_last.x = x0 + t_max * dx/norm;
-            // p_last.y = y0 + t_max * dy/norm;
-            // p_last.z = z0 + t_max * dz/norm;
-
-            // p_milieu.z = z0 + (t_max+t_min)/2 * dz/norm;
-            // p_milieu.y = y0 + (t_max+t_min)/2 * dy/norm;
-            // p_milieu.z = x0 + (t_max+t_min)/2 * dx/norm;
+                geometry_msgs::msg::Point p_first, p_last;
+                //je prend ma ligne qui commence a xo,y,zo 
+                //a changer parce que ca donne des segment decallé mais sur la bonne direction juste pour check 
+                float norm = sqrt(dx*dx + dy*dy + dz*dz);
+                p_first.x = x0 - dx*t/norm/2;
+                p_first.y = y0 - dy*t/norm/2;
+                p_first.z = z0 - dz*t/norm/2;
+                p_last.x = p_first.x + dx*t/norm;
+                p_last.y= p_first.y + dy*t/norm;
+                p_last.z= p_first.z + dz*t/norm;
 
 
 
-            LineDetection det;
-            det.x0 = x0; det.y0 = y0; det.z0 = z0;
-            det.dx = dx; det.dy = dy; det.dz = dz;
-            det.p_first = p_first;
-            det.p_last = p_last;
-            //det.p_milieu = p_milieu
-            //det.t_milieu = (t_max+t_min)/2
+                    // pcl::PointCloud<pcl::PointXYZ> projected_inliers;
+                    // pcl::ProjectInliers<pcl::PointXYZ> proj;
+                    // proj.setModelType(pcl::SACMODEL_LINE);
+                    // proj.setInputCloud(detected_lines);
+                // proj.setModelCoefficients(coefficients);
+                // proj.filter(projected_inliers);
 
-            lines.push_back(det);
+                // std::vector<float> ts;
+                // ts.reserve(projected_inliers.size());
 
-            visualization_msgs::msg::Marker line_marker;
-            line_marker.header = cloud_msg.header;
-            line_marker.ns = "detected_lines";
-            line_marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
-            line_marker.action = visualization_msgs::msg::Marker::ADD;
-            line_marker.scale.x = 0.02; // épaisseur de la ligne
-            line_marker.color.r = 0.0f;
-            line_marker.color.g = 1.0f;
-            line_marker.color.b = 0.0f;
-            line_marker.color.a = 1.0f;
+                // for (const auto &p : projected_inliers.points) {
+                //     float vx = p.x - x0;
+                //     float vy = p.y - y0;
+                //     float vz = p.z - z0;
 
-            line_marker.points.push_back(p_first);
-            line_marker.points.push_back(p_last);
+                //     float t = vx * dx + vy * dy + vz * dz;
+                //     ts.push_back(t);
+                // }
+                // auto [t_min_it, t_max_it] = std::minmax_element(ts.begin(), ts.end());
+                //     float t_min = *t_min_it;
+                //     float t_max = *t_max_it;
+                
+                //geometry_msgs::msg::Point p_first, p_last,p_milieu;
+                // p_first.x = x0 + t_min * dx/norm;
+                // p_first.y = y0 + t_min * dy/norm;
+                // p_first.z = z0 + t_min * dz/norm;
 
-            markers.markers.push_back(line_marker);
+                // p_last.x = x0 + t_max * dx/norm;
+                // p_last.y = y0 + t_max * dy/norm;
+                // p_last.z = z0 + t_max * dz/norm;
+
+                // p_milieu.z = z0 + (t_max+t_min)/2 * dz/norm;
+                // p_milieu.y = y0 + (t_max+t_min)/2 * dy/norm;
+                // p_milieu.z = x0 + (t_max+t_min)/2 * dx/norm;
+
+
+
+                LineDetection det;
+                det.x0 = x0; det.y0 = y0; det.z0 = z0;
+                det.dx = dx; det.dy = dy; det.dz = dz;
+                det.p_first = p_first;
+                det.p_last = p_last;
+                //det.p_milieu = p_milieu
+                //det.t_milieu = (t_max+t_min)/2
+
+                lines.push_back(det);
+
+                visualization_msgs::msg::Marker line_marker;
+                line_marker.header = cloud_msg.header;
+                line_marker.ns = "detected_lines";
+                line_marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
+                line_marker.action = visualization_msgs::msg::Marker::ADD;
+                line_marker.scale.x = 0.01; // épaisseur de la ligne
+                line_marker.color.r = 0.0f;
+                line_marker.color.g = 1.0f;
+                line_marker.color.b = 0.0f;
+                line_marker.color.a = 1.0f;
+
+                line_marker.points.push_back(p_first);
+                line_marker.points.push_back(p_last);
+
+                markers.markers.push_back(line_marker);
+            }//on arete le if 
 
             extract.setNegative(true);
             pcl::PointCloud<pcl::PointXYZ>::Ptr remainder(new pcl::PointCloud<pcl::PointXYZ>());
@@ -201,6 +233,116 @@ private:
         cloud_with_all_lines.header = cloud_msg.header;
         lines_pub_->publish(cloud_with_all_lines);
         markers_pub_->publish(markers);
+
+        geometry_msgs::msg::Point p_centre;
+
+        if (!lines.empty()) {
+            // Vérifier les paires de lignes proches (< 0.6 m entre les milieux)
+            const float distance_threshold_lines = 0.6;
+            int non_prendpasligne1=10;
+            int non_prendpasligne2=10;
+            int non_prendpasligne3=10;
+            for (size_t i = 0; i < lines.size(); ++i) {
+                const auto &L1 = lines[i];
+                // milieu de la ligne i
+                geometry_msgs::msg::Point mid1;
+                mid1.x = 0.5 * (L1.p_first.x + L1.p_last.x);
+                mid1.y = 0.5 * (L1.p_first.y + L1.p_last.y);
+                mid1.z = 0.5 * (L1.p_first.z + L1.p_last.z);
+
+                for (size_t j = i + 1; j < lines.size(); ++j) {
+                    const auto &L2 = lines[j];
+                    geometry_msgs::msg::Point mid2;
+                    mid2.x = 0.5 * (L2.p_first.x + L2.p_last.x);
+                    mid2.y = 0.5 * (L2.p_first.y + L2.p_last.y);
+                    mid2.z = 0.5 * (L2.p_first.z + L2.p_last.z);
+
+                    float dx = mid1.x - mid2.x;
+                    float dy = mid1.y - mid2.y;
+                    float dz = mid1.z - mid2.z;
+                    float dist = sqrt(dx * dx + dy * dy + dz * dz);
+
+                    if (dist < distance_threshold_lines) {
+                        p_centre.x = lines[i].x0 + lines[j].dx * t / 2;
+                        p_centre.y = lines[i].y0 + lines[j].dy * t / 2;
+                        p_centre.z = lines[i].z0 + lines[j].dz * t / 2;
+
+                        float dist_cote_1 = sqrt(lines[i].x0 * lines[i].x0 + lines[i].y0 * lines[i].y0 + lines[i].z0 * lines[i].z0);
+                        float dist_centre_cube = sqrt(p_centre.x * p_centre.x + p_centre.y * p_centre.y + p_centre.z * p_centre.z);
+                        // Si le centre est "avant" le côté, on prend l'autre direction
+                        if (dist_centre_cube < dist_cote_1) {
+                            p_centre.x = lines[i].x0 - lines[j].dx * t / 2;
+                            p_centre.y = lines[i].y0 - lines[j].dy * t / 2;
+                            p_centre.z = lines[i].z0 - lines[j].dz * t / 2;
+                        }
+
+                        if (non_prendpasligne1 == 10){
+                            non_prendpasligne1 = static_cast<int>(j);
+                        }
+                        else if (non_prendpasligne2 == 10){
+                            non_prendpasligne2 = static_cast<int>(j);
+                        }
+                        else if (non_prendpasligne3 == 10){
+                            non_prendpasligne3 = static_cast<int>(j);
+                        }
+
+                        // Marker RViz pour 
+                        visualization_msgs::msg::Marker centre_marker;
+                        centre_marker.header = cloud_msg.header;
+                        centre_marker.ns = "cube_centers";
+                        centre_marker.id = static_cast<int>(i * lines.size() + j);
+                        centre_marker.type = visualization_msgs::msg::Marker::SPHERE;
+                        centre_marker.action = visualization_msgs::msg::Marker::ADD;
+                        centre_marker.pose.position = p_centre;
+                        centre_marker.pose.orientation.x = 0.0;
+                        centre_marker.pose.orientation.y = 0.0;
+                        centre_marker.pose.orientation.z = 0.0;
+                        centre_marker.pose.orientation.w = 1.0;
+                        centre_marker.scale.x = 0.08;
+                        centre_marker.scale.y = 0.08;
+                        centre_marker.scale.z = 0.08;
+                        centre_marker.color.r = 1.0f;
+                        centre_marker.color.g = 0.0f;
+                        centre_marker.color.b = 0.0f;
+                        centre_marker.color.a = 1.0f;
+
+                        points.markers.push_back(centre_marker);
+                    }
+                    else{
+                        if (j==lines.size()-1 && i!=non_prendpasligne1 && i!=non_prendpasligne2 && i!=non_prendpasligne3) {
+                            p_centre.x = lines[i].x0 - lines[j].dy * t / 2;
+                            p_centre.y = lines[i].y0 + lines[j].dx * t / 2;
+                            p_centre.z = lines[i].z0 ;
+
+                            // Marker RViz 
+                            visualization_msgs::msg::Marker centre_marker;
+                            centre_marker.header = cloud_msg.header;
+                            centre_marker.ns = "cube_centers_alt";
+                            centre_marker.id = static_cast<int>(i * lines.size() + j);
+                            centre_marker.type = visualization_msgs::msg::Marker::SPHERE;
+                            centre_marker.action = visualization_msgs::msg::Marker::ADD;
+                            centre_marker.pose.position = p_centre;
+                            centre_marker.pose.orientation.x = 0.0;
+                            centre_marker.pose.orientation.y = 0.0;
+                            centre_marker.pose.orientation.z = 0.0;
+                            centre_marker.pose.orientation.w = 1.0;
+                            centre_marker.scale.x = 0.08;
+                            centre_marker.scale.y = 0.08;
+                            centre_marker.scale.z = 0.08;
+                            centre_marker.color.r = 0.0f;
+                            centre_marker.color.g = 0.0f;
+                            centre_marker.color.b = 1.0f;
+                            centre_marker.color.a = 1.0f;
+
+                            points.markers.push_back(centre_marker);
+
+                        }
+
+                    }
+                }
+            }
+        }
+
         // geometry_msgs::msg::Point p_a_tester_1, p_a_tester_2;
 
         // for (size_t i = 0; i < lines.size(); ++i) {
@@ -245,6 +387,7 @@ private:
 
 
             
+        centre_pub_->publish(points);
 
 
 
@@ -272,11 +415,13 @@ private:
     int max_iterations_;
     int min_inliers_;
     int max_lines_;
-    int t ;
+    float t ;
 
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_subscriber_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr lines_pub_;
-    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr markers_pub_;
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr markers_pub_;      
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr centre_pub_;
+
 
 };
 
